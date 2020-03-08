6e5b711a32163db30ed76f7d07364a93a213df28
Friedrich von Never <friedrich@fornever.me>
2020-03-08 23:00:44 +0700

    Add a simple command line argument system

--- Fenrir/Program.fs
+++ Fenrir/Program.fs
@@ -1,6 +1,39 @@
 ﻿open System
+open System.Reflection
+
+let private printVersion() =
+    let version = Assembly.GetExecutingAssembly().GetName().Version
+    printfn "v%A" version
+
+let private printUsage() =
+    printf @"
+Usage:
+
+  version
+  --version
+    Print the program version.
+
+  help
+  --help
+    Print this message.
+"
+
+let private printUnrecognizedArguments argv =
+    printfn "Arguments were not recognized: %A" argv
+
+module ExitCodes =
+    let Success = 0
+    let UnrecognizedArguments = 1
 
 [<EntryPoint>]
-let main argv =
-    printfn "Fenrir ver 0.0"
-    0
+let main (argv: string[]): int =
+    match argv with
+    | [|"version"|] | [|"--version"|] ->
+        printVersion()
+        ExitCodes.Success
+    | [|"help"|] | [|"--help"|] | [||] ->
+        printUsage()
+        ExitCodes.Success
+    | _ ->
+        printUnrecognizedArguments argv
+        ExitCodes.UnrecognizedArguments
