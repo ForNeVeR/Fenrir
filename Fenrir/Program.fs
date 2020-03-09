1a6ad4b6eb63ece207f000f193a327469861aa43
Friedrich von Never <friedrich@fornever.me>
2020-03-09 18:16:50 +0700

    Integrate object-type with the command system (#5)

--- Fenrir/Program.fs
+++ Fenrir/Program.fs
@@ -4,6 +4,8 @@ open System
 open System.IO
 open System.Reflection
 
+open Fenrir
+
 let private printVersion() =
     let version = Assembly.GetExecutingAssembly().GetName().Version
     printfn "v%A" version
@@ -14,6 +16,11 @@ Usage:
 
   (help | --help)
     Print this message.
+
+  object-type [<path>]
+    Prints the type of the Git raw object read from the file system.
+
+    If <path> isn't passed, then accepts raw object contents from the standard input.
 
   unpack [<input> [<output>]]
     Unpacks the object file passed as <input> and writes the results to the <output>.
@@ -34,6 +41,15 @@ module ExitCodes =
 [<EntryPoint>]
 let main (argv: string[]): int =
     match argv with
+    | [|"object-type"|] ->
+        use input = Console.OpenStandardInput()
+        Commands.printObjectPath input
+        ExitCodes.Success
+    | [|"object-type"; inputFilePath|] ->
+        use input = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read, FileShare.Read)
+        Commands.printObjectPath input
+        ExitCodes.Success
+
     | [|"help"|] | [|"--help"|] | [||] ->
         printUsage()
         ExitCodes.Success
