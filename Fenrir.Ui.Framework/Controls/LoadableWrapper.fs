99cc0056090374b2de8da965f58d5197b0b0f259
Friedrich von Never <friedrich@fornever.me>
2020-05-10 15:22:23 +0700

    Proper center alignment for the preloader (#9)

--- Fenrir.Ui.Framework/Controls/LoadableWrapper.fs
+++ Fenrir.Ui.Framework/Controls/LoadableWrapper.fs
@@ -2,6 +2,7 @@
 
 open ConsoleFramework.Controls
 open ConsoleFramework.Core
+open ConsoleFramework.Rendering
 open Xaml
 
 /// This is a control that is able to show a placeholder with "Loading" word instead of its content.
@@ -9,9 +10,7 @@ open Xaml
 type LoadableWrapper() =
     inherit Control()
 
-    let placeholder = TextBlock(Text = "Loading…",
-                                VerticalAlignment = VerticalAlignment.Stretch,
-                                HorizontalAlignment = HorizontalAlignment.Stretch)
+    let loadingText = "Loading…"
 
     [<VolatileField>]
     let mutable loading = true
@@ -25,20 +24,36 @@ type LoadableWrapper() =
     member _.IsLoading with get() = loading
     member this.IsLoading with set(value) =
         loading <- value
-        placeholder.Visibility <- if value then Visibility.Visible else Visibility.Collapsed
         if not <| isNull content then
             content.Visibility <- if value then Visibility.Collapsed else Visibility.Visible
         this.Invalidate()
 
-    member private this.ActiveControl: Control = if loading then upcast placeholder else this.Content
-
     override this.MeasureOverride(availableSize) =
-        this.ActiveControl.Measure availableSize
-        this.ActiveControl.DesiredSize
+        if loading then Size(loadingText.Length, 1)
+        else
+            content.Measure availableSize
+            content.DesiredSize
 
     override this.ArrangeOverride(finalSize) =
-        this.ActiveControl.Arrange(Rect(finalSize))
+        if not loading then content.Arrange(Rect(finalSize))
         finalSize
 
+    member private this.RenderLoadingPlaceholder(buffer: RenderingBuffer) =
+        let attr = Colors.Blend(Color.Black, Color.DarkYellow)
+        let actualWidth = this.ActualWidth
+        let actualHeight = this.ActualHeight
+        buffer.FillRectangle(0, 0, actualWidth, actualHeight, ' ', attr)
+        let offset =
+            let x = max 0 (actualWidth - loadingText.Length) / 2
+            let y = actualHeight / 2
+            Vector(x, y)
+        for x in 0..actualWidth do
+            for y in 0..actualHeight do
+                if y = 0 && x < loadingText.Length then
+                    buffer.SetPixel(offset.X + x, offset.Y + y, loadingText.[x], attr)
+
+        buffer.SetOpacityRect(0, 0, actualWidth, actualHeight, 3)
+
     override this.Render(buffer) =
-        this.ActiveControl.Render buffer
+        if loading then this.RenderLoadingPlaceholder buffer
+        else content.Render buffer
