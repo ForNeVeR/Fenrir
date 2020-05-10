ce72e585f49e04c5f2b2bef054c51e3429bf5c73
Friedrich von Never <friedrich@fornever.me>
2020-05-10 18:59:02 +0700

    Cache refs to array to prevent any IO on UI thread (#32)

--- Fenrir/Ui/GitRepositoryModel.fs
+++ Fenrir/Ui/GitRepositoryModel.fs
@@ -5,5 +5,6 @@ open Fenrir
 type GitRepositoryModel(gitDirectoryPath: string) =
     member _.ReadRefsAsync(): Async<string seq> = async {
         return Refs.readRefs gitDirectoryPath
+               |> Seq.toArray
                |> Seq.map(fun ref -> ref.Name)
     }
