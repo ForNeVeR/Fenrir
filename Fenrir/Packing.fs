e07b1f95dd7f226051283fd6de5f0b1f2ad61bd3
Daniel-mon-Goose <danny-rosso@yandex.ru>
2020-07-23 01:50:04 +0500

    added sha1 array extraction from .idx file

--- Fenrir/Packing.fs
+++ Fenrir/Packing.fs
@@ -2,13 +2,28 @@
 
 open System
 open System.IO
-open System.Text
 
-let anotherEndian (reader: BinaryReader) : uint32 =
-    let bytes = reader.ReadBytes 4 |> Array.map (fun x -> uint32 x)
-    bytes |> Array.reduce (fun x y -> x * (uint32 256) + y)
+let anotherEndian (reader: BinaryReader) : int =
+    reader.ReadBytes 4
+        |> Array.map (fun x -> int x)
+        |> Array.reduce (fun x y ->
+            x * 256 + y)
 
-let parseIndex (path: String) (hash: String) : uint32 =
+let readHash (reader: BinaryReader) : String =
+    reader.ReadBytes 20
+        |> Array.fold (fun acc elem ->
+            String.Concat(acc, sprintf "%02x" elem)) ""
+
+let parseIndex (path: String) (hash: String) : int =
+    let sizePosition = 1028L
     let idxReader = new BinaryReader(File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read))
-    idxReader.BaseStream.Position <- 1028L
-    anotherEndian idxReader
+    idxReader.BaseStream.Position <- sizePosition
+
+    let size = anotherEndian idxReader
+    let hashes = Array.zeroCreate size
+    seq{0..size - 1}
+        |> Seq.iter (fun i ->
+            hashes.[i] <- readHash idxReader)
+    hashes.[0] |> printfn "%s"
+    hashes.[1] |> printfn "%s"
+    size
