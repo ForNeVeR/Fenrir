1369e67469bd9ab7d60a77d7b0d2905fee492cef
Daniel-mon-Goose <danny-rosso@yandex.ru>
2020-07-25 18:51:16 +0500

    implemented pack file parsing without deltas processing

--- Fenrir/Packing.fs
+++ Fenrir/Packing.fs
@@ -1,7 +1,15 @@
-﻿module Fenrir.packing
+﻿module Fenrir.Packing
 
 open System
 open System.IO
+
+let flagToBits(key: String) : byte[] =
+    match key with
+        | "commit" -> [| 0uy; 0uy; 1uy |]
+        | "tree"   -> [| 0uy; 1uy; 0uy |]
+        | "blob"   -> [| 0uy; 1uy; 1uy |]
+        | "tag"    -> [| 1uy; 0uy; 0uy |]
+        | _        -> failwithf "Unknown object type provided"
 
 let anotherEndian (reader: BinaryReader) : int =
     reader.ReadBytes 4
@@ -13,6 +21,22 @@ let readHash (reader: BinaryReader) : String =
     reader.ReadBytes 20
         |> Array.fold (fun acc elem ->
             String.Concat(acc, sprintf "%02x" elem)) ""
+
+let byteToBits (num: byte) : byte[] =
+    let mutable number = num
+    Array.init 8 (fun _ ->
+        let result = number % 2uy
+        number <- number >>> 1
+        result)
+
+
+let bitsToInt (bits: byte[]) =
+    let mutable st = 1
+
+    Array.foldBack (fun i acc ->
+        let result = acc + int i * st
+        st <- st * 2
+        result) bits 0
 
 let parseIndexOffset (path: String) (hash: String) : int =
     let idxReader = new BinaryReader(File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read))
@@ -30,3 +54,23 @@ let parseIndexOffset (path: String) (hash: String) : int =
         + (int64 pos) * 4L
 
     anotherEndian idxReader
+
+let parsePackInfo (path: String) (offset: int) (flag: String) : MemoryStream =
+    let bitsExtractor (reader: BinaryReader) =
+        reader.ReadByte() |> byteToBits |> Array.rev
+
+    let packReader = new BinaryReader(File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+    packReader.BaseStream.Position <- int64 offset
+    let mutable bits = bitsExtractor packReader
+    let mutable size = bits.[4..7] |> bitsToInt
+    let mutable sizeOffset = 4
+
+    //TODO implement delta objs
+    if bits.[1..3] = flagToBits(flag) then
+        while bits.[0] = 1uy do
+            bits <- bitsExtractor packReader
+            size <- bits.[1..] |> bitsToInt <<< sizeOffset ||| size
+            sizeOffset <- sizeOffset + 7
+    else
+        failwithf "wrong type of file provided"
+    new MemoryStream(size + 50 |> packReader.ReadBytes)
