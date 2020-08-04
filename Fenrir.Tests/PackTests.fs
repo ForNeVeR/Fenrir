c6f4bb4506ace512403c892ac3d101069b38939b
Daniel-mon-Goose <danny-rosso@yandex.ru>
2020-08-04 20:15:00 +0500

    changed arrays to string tests

--- Fenrir.Tests/PackTests.fs
+++ Fenrir.Tests/PackTests.fs
@@ -1,6 +1,7 @@
 ﻿module Fenrir.Tests.PackTests
 
 open System.IO
+open System.Text
 open Xunit
 
 open Fenrir.Tests.TestUtils
@@ -8,7 +9,10 @@ open Fenrir.Packing
 
 [<Fact>]
 let ``Packed commit should be parsed properly``(): unit =
+    let toString (arr: byte array) =
+        arr |> Encoding.ASCII.GetString
+
     let commitHash = "8c3ecc6b9abdab719915046ce7e989715fde5f5b"
     use content = getPackedStream testDataRoot commitHash "commit"
     let expected = File.ReadAllBytes(Path.Combine(testDataRoot, "commit"))
-    Assert.Equal<byte>(expected, content.ToArray())
+    Assert.Equal<string>(expected |> toString, content.ToArray() |> toString)
