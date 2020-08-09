ff247c54f5a38de4ce1e15f13f4453a0c47e0bd0
Daniel-mon-Goose <danny-rosso@yandex.ru>
2020-08-10 00:24:50 +0500

    added HEAD and refs modification to update-commit

--- Fenrir/ArgumentCommands.fs
+++ Fenrir/ArgumentCommands.fs
@@ -14,20 +14,30 @@ let unrecognizedArgs(argv: string[]): int =
     ExitCodes.UnrecognizedArguments
 
 let updateCommitOp (commitHash: string) (pathToRepo: string) (filePath: string) (detachedAllowed: bool): int =
-    let pathToDotGit = Path.Combine(pathToRepo, ".git")
-    let fullPathToFile = Path.Combine(pathToRepo, filePath)
-    let oldCommit = Commands.parseCommitBody pathToDotGit commitHash
-    let oldRootTreeHash = oldCommit.Tree
-    use inputBlob = new FileStream(fullPathToFile, FileMode.Open, FileAccess.Read, FileShare.Read)
-    use headedBlob = new MemoryStream()
-    let blobHash = Commands.headifyStream Commands.GitObjectType.GitBlob inputBlob headedBlob
-    Commands.writeStreamToFile pathToRepo headedBlob blobHash
-    use treeStreams = Commands.updateObjectInTree oldRootTreeHash pathToDotGit filePath blobHash
-    let newRootTreeHash = treeStreams.Hashes.[0]
-    let newCommit = Commands.changeHashInCommit oldCommit (newRootTreeHash |> Tools.stringToByte)
-    use inputCommit = Commands.commitBodyToStream newCommit |> Commands.doAndRewind
-    use headedCommit = new MemoryStream()
-    let commitHash = Commands.headifyStream Commands.GitObjectType.GitCommit inputCommit headedCommit
-    Commands.writeStreamToFile pathToRepo headedCommit commitHash
-    Commands.writeTreeObjects pathToRepo treeStreams
+    if not detachedAllowed && Refs.isHeadDetached pathToRepo then
+        printfn "You are in detached head mode. Any repo modifications may turn your repo FUBAR.\n
+        If you ARE willing to seek adventure through Stackoverflow or aware of what you are doing,
+        provide --force key.\n
+        If at any moment your repo has turned FUBAR, consider consulting 'git log --reflog' option
+        in your Git terminal to locate your missing commits."
+    else
+        let pathToDotGit = Path.Combine(pathToRepo, ".git")
+        let fullPathToFile = Path.Combine(pathToRepo, filePath)
+        let oldCommit = Commands.parseCommitBody pathToDotGit commitHash
+        let oldRootTreeHash = oldCommit.Tree
+        use inputBlob = new FileStream(fullPathToFile, FileMode.Open, FileAccess.Read, FileShare.Read)
+        use headedBlob = new MemoryStream()
+        let blobHash = Commands.headifyStream Commands.GitObjectType.GitBlob inputBlob headedBlob
+        Commands.writeStreamToFile pathToRepo headedBlob blobHash
+        use treeStreams = Commands.updateObjectInTree oldRootTreeHash pathToDotGit filePath blobHash
+        let newRootTreeHash = treeStreams.Hashes.[0]
+        let newCommit = Commands.changeHashInCommit oldCommit (newRootTreeHash |> Tools.stringToByte)
+        use inputCommit = Commands.commitBodyToStream newCommit |> Commands.doAndRewind
+        use headedCommit = new MemoryStream()
+        let newCommitHash = Commands.headifyStream Commands.GitObjectType.GitCommit inputCommit headedCommit
+        Commands.writeStreamToFile pathToRepo headedCommit newCommitHash
+        Commands.writeTreeObjects pathToRepo treeStreams
+
+        Refs.updateHead commitHash newCommitHash pathToDotGit
+        Refs.updateAllRefs commitHash newCommitHash pathToDotGit
     ExitCodes.Success
