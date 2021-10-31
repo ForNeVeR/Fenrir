448cc4786317f197d567c34cb6a75f0afd181528
Stas <stastmn@gmail.com>
2021-10-31 19:27:29 +0300

    Update test for packed refs reading. Also Added packed-refs file to test data.

--- Fenrir.Tests/RefsTests.fs
+++ Fenrir.Tests/RefsTests.fs
@@ -1,4 +1,4 @@
-﻿module Fenrir.Tests.RefsTests
+module Fenrir.Tests.RefsTests
 
 open Xunit
 
@@ -13,15 +13,21 @@ let ``Attached head should be recognized properly``(): unit =
 let ``Detached head should be recognized properly``(): unit =
     Assert.True(Refs.isHeadDetached("Data2"))
 
+    
 [<Fact>]
-let ``Ref list should be read properly``(): unit =
+let ``Ref list should be read properl``():unit =
     let refs = Refs.readRefs testDataRoot
     let expectedRefs = [|
         { Name = "refs/heads/feature/feature-name"; CommitObjectId = "cc07136d669554cf46ca4e9ef1eab7361336e1c8" }
         { Name = "refs/heads/master"; CommitObjectId = "cc07136d669554cf46ca4e9ef1eab7361336e1c8" }
         { Name = "refs/remotes/origin/master"; CommitObjectId = "cc07136d669554cf46ca4e9ef1eab7361336e1c8" }
+        { Name = "refs/tags/apr2016"; CommitObjectId = "953b93514f32c580081b81be9e2918214e9891a2" }
+        { Name = "refs/tags/apr2017"; CommitObjectId = "77969da813b975ff6b7805814bb3c959cbcc1d6c" }
+        { Name = "refs/tags/apr2018"; CommitObjectId = "6ffb8ec42b04d0f9334c1799338a0fa73381ad3d" }
+
     |]
     Assert.Equal<Ref>(expectedRefs, refs)
+    
 
 [<Fact>]
 let ``Refs should be identified properly``(): unit =
