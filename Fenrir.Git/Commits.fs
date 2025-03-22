f92a003a73b94dae32fd3c841c05ec46dddafa53
Friedrich von Never <friedrich@fornever.me>
2025-03-22 23:16:08 +0100

    Legal: licenses for the new files

--- Fenrir.Git/Commits.fs
+++ Fenrir.Git/Commits.fs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Friedrich von Never <friedrich@fornever.me>
+//
+// SPDX-License-Identifier: MIT
+
 /// Functions to manipulate Git commits.
 module Fenrir.Git.Commits
 
